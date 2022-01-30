dd85e234b0026ee0b9664255026706155e769883
hiroyuki.miyauchi <hiroyuki.miyauchi0410@gmail.com>
2022-01-30 14:42:33 +0900

    コピーライト部分の変更

--- footer.php
+++ footer.php
@@ -33,7 +33,7 @@
       </ul>
 
       <div class="footer_copyright">
-        <small>&copy; 2020 - <?php echo date('Y'); // 現在の西暦を取得 ?> <?php bloginfo('name'); // サイトのタイトルを取得 ?></small>
+        <small>&copy; 2020 - <?php echo date('Y'); // 現在の西暦を取得 ?> misachisalon</small>
       </div>
     </div>
   </div>
