e9f143d1214ff2a15d785d87eb442b2e3f824767
hiroyuki.miyauchi <hiroyuki.miyauchi0410@gmail.com>
2022-01-30 21:21:31 +0900

    各CSSのバージョンを変更（キャッシュクリア対策）

--- header.php
+++ header.php
@@ -20,16 +20,16 @@
 
 <?php // CSS（全ページ読み込み）
 wp_enqueue_style('misachisalon-default', get_template_directory_uri() . '/assets/css/default.min.css', array(), '1.0.0');
-wp_enqueue_style('misachisalon-common', get_template_directory_uri() . '/assets/css/common.min.css', array(), '1.0.3');
+wp_enqueue_style('misachisalon-common', get_template_directory_uri() . '/assets/css/common.min.css', array(), '1.0.4');
 ?>
 <?php // CSS（条件に一致するページ毎に読み込み）
 if ( is_home() ) { // homeのみ読み込み
-  wp_enqueue_style('misachisalon-home', get_template_directory_uri() . '/assets/css/home.min.css', array(), '1.0.3');
+  wp_enqueue_style('misachisalon-home', get_template_directory_uri() . '/assets/css/home.min.css', array(), '1.0.4');
 } else if ( is_page('profile') ) { // profileのみ読み込み
-  wp_enqueue_style('misachisalon-contents', get_template_directory_uri() . '/assets/css/contents.min.css', array(), '1.0.5');
+  wp_enqueue_style('misachisalon-contents', get_template_directory_uri() . '/assets/css/contents.min.css', array(), '1.0.6');
   wp_enqueue_style('misachisalon-profile', get_template_directory_uri() . '/assets/css/profile.min.css', array(), '1.0.1');
 } else { // 「home」や「profile」以外の全ページ読み込み
-  wp_enqueue_style('misachisalon-contents', get_template_directory_uri() . '/assets/css/contents.min.css', array(), '1.0.5');
+  wp_enqueue_style('misachisalon-contents', get_template_directory_uri() . '/assets/css/contents.min.css', array(), '1.0.6');
 }
 ?>
 <?php // CSS（全ページ読み込み）
